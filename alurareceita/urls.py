26a0ec00c215a656ae2c2d843ec600f176cef88e
daianeklein <daiane.klein22@gmail.com>
2022-08-02 23:10:02 -0300

    add files

---
+++ alurareceita/urls.py
@@ -0,0 +1,10 @@
+"""alurareceita URL Configuration"""
+
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('receitas.urls')),
+    path('admin/', admin.site.urls),
+]
