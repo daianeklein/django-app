07b1e4da0775a27d4e251d4c76eddf42c36004a9
daianeklein <daiane.klein22@gmail.com>
2022-08-04 08:30:11 -0300

    update files

--- receitas/views.py
+++ receitas/views.py
@@ -4,7 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa de Legumes',
+        3: 'Sorvete de Morango',
+        4: 'Sufle de Batata'
+    }
+    dados = {
+        'nome_das_receitas' : receitas
+    }
+    return render(request,'index.html', dados)
 
 def receita(request):
     return render(request,'receita.html')   
