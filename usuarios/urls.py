46d7ba532cb1a8b02ddfe0b64570f9a796bc2a4a
daianeklein <daiane.klein22@gmail.com>
2022-08-09 21:14:46 -0300

    created usuarios app

---
+++ usuarios/urls.py
@@ -0,0 +1,11 @@
+from xml.etree.ElementInclude import include
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('cadastro', views.cadastro, name='cadastro'),
+    path('login', views.login, name = 'login'),
+    path('dashboard', views.dashboard, name ='dashboard'),
+    path('logout', views.logout, name='logout'),
+
+]
