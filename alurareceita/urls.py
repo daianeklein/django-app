bcd1274f4dd4511fd794901b32c37410bfec5fe2
daianeklein <daiane.klein22@gmail.com>
2022-08-16 09:40:26 -0300

    add files

--- alurareceita/urls.py
+++ alurareceita/urls.py
@@ -1,9 +1,6 @@
-"""alurareceita URL Configuration"""
-
-from xml.etree.ElementInclude import include
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -11,4 +8,3 @@ urlpatterns = [
     path('usuarios/', include('usuarios.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
