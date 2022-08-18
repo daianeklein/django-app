3c1c1119fb95ded690af4f9f9cee5330b7282ac4
daianeklein <daiane.klein22@gmail.com>
2022-08-18 18:09:09 -0300

    form config

--- 02-to_do_app/todo_list/base/views.py
+++ 02-to_do_app/todo_list/base/views.py
@@ -4,6 +4,8 @@ from django.forms import models
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
 #from django.http import HttpResponse
@@ -17,3 +19,7 @@ class TaskDetail(DetailView):
     context_object_name = 'task'
     template_name = 'base/task.html'
 
+class TaskCreate(CreateView):
+    model = Task
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
