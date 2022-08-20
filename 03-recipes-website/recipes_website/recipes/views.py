fd04bf75c53a41bd6e98a54cddf49770850f9498
daianeklein <daiane.klein22@gmail.com>
2022-08-20 14:44:38 -0300

    recipes page config

--- 03-recipes-website/recipes_website/recipes/views.py
+++ 03-recipes-website/recipes_website/recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Recipe
 
 # def index(request):
 #     return HttpResponse('<h1>Recipes</h1>')
@@ -8,5 +9,10 @@ def index(request):
     return render(request,'index.html')
 
 def view_recipes_page(request):
-    return render(request, 'recipes.html')
+    recipes = Recipe.objects.all()
+
+    data_recipes = {
+        'recipes' : recipes 
+    }
+    return render(request, 'recipes.html', data_recipes)
 
