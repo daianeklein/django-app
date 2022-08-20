cae3c93bf0ba4aedb7ced2604eac5db174bd14d9
daianeklein <daiane.klein22@gmail.com>
2022-08-20 13:28:27 -0300

    add new files - migrations

--- 03-recipes-website/recipes_website/recipes/models.py
+++ 03-recipes-website/recipes_website/recipes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=100)
+    ingredients = models.TextField()
+    directions = models.TextField()
+    prep = models.IntegerField()
+    servings = models.CharField(max_length=20)
+    date_recipe = models.DateTimeField(default=datetime.now, blank=True)
--- 03-recipes-website/recipes_website/recipes_website/settings.py
+++ 03-recipes-website/recipes_website/recipes_website/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'recipes_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'recipes-website',
+        'USER' : 'postgres',
+        'PASSWORD' : '220891',
+        'HOST' : 'localhost'
+
     }
 }
 
