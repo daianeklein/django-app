fb4b5007a2037b66dfae1618dd0ea7df0545dbb5
daianeklein <daiane.klein22@gmail.com>
2022-09-14 11:29:59 -0300

    form style

--- 04-form-validation/passagens/views.py
+++ 04-form-validation/passagens/views.py
@@ -1,5 +1,15 @@
+import formatter
 from django.shortcuts import render
+from passagens.forms import PassagemForms
 
 def index(request):
-    return render(request, 'index.html')
+    form = PassagemForms()
+    contexto = {'form' : form}
+    return render(request, 'index.html', contexto)
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForms(request.POST)
+        contexto = {'form' : form}
+        return render(request, 'minha_consulta.html', contexto)
 
--- 04-form-validation/setup/settings.py
+++ 04-form-validation/setup/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tempus_dominus',
     'passagens',
     'django.contrib.admin',
     'django.contrib.auth',
