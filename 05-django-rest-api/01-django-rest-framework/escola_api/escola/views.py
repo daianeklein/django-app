7b9d94ad73835ef51ee4210bdacc7581959629bf
Daiane Klein <daiane.klein22@gmail.com>
2022-10-22 23:10:29 -0300

    api config done

--- 05-django-rest-api/01-django-rest-framework/escola_api/escola/views.py
+++ 05-django-rest-api/01-django-rest-framework/escola_api/escola/views.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
-from .serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
+from .serializer import AlunoSerializer, CursoSerializer, ListaMatriculasAlunoSerializer, MatriculaSerializer, ListaAlunosMatriculadosSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # from django.http import JsonResponse
 
@@ -14,6 +16,8 @@ class AlunosViewSet(viewsets.ModelViewSet):
     '''Exibe todos os alunos e alunas'''
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    authentication_classes =  [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class CursosViewSet(viewsets.ModelViewSet):
     '''Exibe todos os cursos'''
@@ -24,4 +28,22 @@ class MatriculaViewSet(viewsets.ModelViewSet):
     '''Lista todas as matrículas'''
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+
+class ListaMetriculasAlunos(generics.ListAPIView):
+    '''Lista as matrículas de um aluno ou aluna'''
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id = self.kwargs['pk'])
+        return queryset
+
+    serializer_class = ListaMatriculasAlunoSerializer
+
+
+class ListaALunosMatriculados(generics.ListAPIView):
+    '''Lista alunos e alunas matriculados em um curso'''
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id = self.kwargs['pk'])
+
+        return queryset
+
+    serializer_class = ListaAlunosMatriculadosSerializer
 
--- 05-django-rest-api/01-django-rest-framework/escola_api/escola_api/urls.py
+++ 05-django-rest-api/01-django-rest-framework/escola_api/escola_api/urls.py
@@ -1,8 +1,12 @@
 
+#from escola.views import ListaALunosMatriculados
+from escola.views import ListaALunosMatriculados
+
+from escola.models import Matricula
 from django.urls import path, include
 from django.contrib import admin
 from django.urls import path
-from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet
+from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet, ListaMetriculasAlunos
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -12,5 +16,7 @@ router.register('matriculas', CursosViewSet, basename= 'Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('aluno/<int:pk>/matriculas/', ListaMetriculasAlunos.as_view()),
+    path('curso/<int:pk>/matriculas/', ListaALunosMatriculados.as_view())
 ]
