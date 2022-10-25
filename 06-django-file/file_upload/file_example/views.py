78b5099ec4d29b39eb8b12f1615f05f4f1306745
Daiane Klein <daiane.klein22@gmail.com>
2022-10-25 07:55:35 -0300

    new files

--- 06-django-file/file_upload/file_example/views.py
+++ 06-django-file/file_upload/file_example/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from .forms import UploadFileForm
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
 from django.core.files.storage import FileSystemStorage
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 def home(request):
     return render(request, 'home.html')
@@ -10,5 +15,14 @@ def upload(request):
         uploaded_file = request.FILES['document']
         fs = FileSystemStorage()
         fs.save(uploaded_file.name, uploaded_file)
-
     return render(request, 'upload.html')
+
+def uploaded_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        file = request.FILES['file']
+        return HttpResponse("Great! " + str(file))
+    else:
+        form = UploadFileForm()
+    return render(request, 'home.html', {'form' : form})
+
--- 06-django-file/file_upload/file_upload/urls.py
+++ 06-django-file/file_upload/file_upload/urls.py
@@ -2,12 +2,17 @@ from django.contrib import admin
 from django.urls import path
 from file_example import views
 from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
-    path('upload/', views.upload, name = 'upload')
+    path('upload/', views.upload, name = 'upload'),
+    path('file_successfully_uploaded/', views.uploaded_file, name = 'file_successfully_uploaded')
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root = settings.MEDIA_ROOT,
+        )
